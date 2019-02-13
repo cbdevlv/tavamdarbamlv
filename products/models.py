b961d098a7f2b171536715dd167539ee9b5a7965
austris <austris.laizans@gmail.com>
2019-02-13 18:23:18 +0200

    Products model created Basic homepage added Superuser added

---
+++ products/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=150)
+    slug = models.SlugField()
+    description = models.TextField()
+    image = models.ImageField(default='default.png', blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    """
+- izmērs
+- tips
+    """
+    def __string__(self):
+        return self.title
+
+
+
+"""When model is changed do:
+python manage.py makemigrations
+python manage.py migrate"""
