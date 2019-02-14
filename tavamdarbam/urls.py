4340bbd0732a96edfd7e618e780b4c91f70b4dfe
austris <austris.laizans@gmail.com>
2019-02-14 14:48:48 +0200

    Products are visible in homepage

--- tavamdarbam/urls.py
+++ tavamdarbam/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from products import views as product_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.homepage),
+    path('', product_views.product_list, name="home"),
 ]
