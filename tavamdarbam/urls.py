066b0c56bff6280727d70f5d406fcd3a6911bb95
austris <austris.laizans@gmail.com>
2019-03-11 20:23:15 +0200

    Fixed picture defect Added default.png

--- tavamdarbam/urls.py
+++ tavamdarbam/urls.py
@@ -17,8 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 from products import views as product_views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', product_views.product_list, name="home"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
