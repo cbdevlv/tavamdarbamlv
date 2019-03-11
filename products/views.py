7bae7007b1ab6e0c4bd6ffbf896539885711b882
austris <austris.laizans@gmail.com>
2019-03-11 19:06:59 +0200

    Added color name under product

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 import csv, io
 #from django.contrib.auth.decorations import permisions_required
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Size
 from django.http import HttpResponse
 
 def product_list(request):
